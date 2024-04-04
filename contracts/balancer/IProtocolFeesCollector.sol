35023ab3e06e8c66fdcf1f8949c86833801a5dac
tHeStRyNg <info@algorithmic.one>
2024-04-04 19:57:31 +0000

    Initial

--- contracts/balancer/IProtocolFeesCollector.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-interface IProtocolFeesCollector {
-    function getSwapFeePercentage() external view returns (uint256);
-
-    function getFlashLoanFeePercentage() external view returns (uint256);
-}
